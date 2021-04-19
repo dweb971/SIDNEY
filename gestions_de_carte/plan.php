debbbe2c4210e728148c5c7f39b8603a313663b7
SIDNEY Lionel <rsma.simplon21@gmail.com>
2021-04-19 11:08:40 -0400

    gestions_de_carte

---
+++ gestions_de_carte/plan.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang="fr">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1,
+            shrink-to-fit=no">
+
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet"
+            href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
+            integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2"
+            crossorigin="anonymous">
+        <link rel="stylesheet" href="style.css">
+        <title></title>
+    </head>
+    <body>
+
+            <!-- Content here -->
+            <header>
+                <div class="container" id="lionel">
+                    <div class="row">
+                        <div class="col-sm">
+                            <div>
+                                <img
+                                    src="top.png" 
+                                    alt="logo">
+                               </div>
+                               <img
+                               src="communes-Guadeloupe-971.jpg" 
+                               alt="commune" width="850px">
+                               <form>
+                                <div class="mb-2">
+                                  <label for="exampleInputEmail1" class="form-label"></label>
+                                  <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"> 
+                                 
+                                </div>
+                                <button type="button" class="btn btn-primary">Modifier</button>
+<button type="button" class="btn btn-secondary">Imprimer</button>
+<button type="button" class="btn btn-success">Télécharger</button>
---
+++ gestions_de_carte/supprimer.php
@@ -0,0 +1,102 @@
+<!doctype html>
+<html lang="fr">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1,
+            shrink-to-fit=no">
+
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet"
+            href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
+            integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2"
+            crossorigin="anonymous">
+        <link rel="stylesheet" href="style.css">
+        <title></title>
+    </head>
+    <body>
+
+            <!-- Content here -->
+            <header>
+                <div class="container" id="lionel">
+                    <div class="row">
+                        <div class="col-sm">
+                            <div>
+                                <img
+                                    src="top.png" 
+                                    alt="logo">
+                                    <div>
+                                
+                                    </div>
+
+                                    <h2>Suprimer un salaries/Suspendre l'accès</h2>
+                                    <table class="table">
+                                        <thead class="thead-dark">
+                                          <tr>
+                                            <th scope="col">#</th>
+                                            <th scope="col">Nom</th>
+                                            <th scope="col">Prénom</th>
+                                            <th scope="col">Handle</th>
+                                          </tr>
+                                        </thead>
+                                        <tbody>
+                                          <tr>
+                                            <th scope="row">1</th>
+                                            <td>Mark</td>
+                                            <td>Otto</td>
+                                            <td>@mdo</td>
+                                          </tr>
+                                          <tr>
+                                            <th scope="row">2</th>
+                                            <td>Jacob</td>
+                                            <td>Thornton</td>
+                                            <td>@fat</td>
+                                          </tr>
+                                          <tr>
+                                            <th scope="row">3</th>
+                                            <td>Larry</td>
+                                            <td>the Bird</td>
+                                            <td>@twitter</td>
+                                          </tr>
+                                        </tbody>
+                                      </table>
+                                      
+                                      <table class="table">
+
+                                          <tr>
+                                            <th scope="row">4</th>
+                                            <td>Jacob</td>
+                                            <td>Thornton</td>
+                                            <td>@fat</td>
+                                          </tr>
+                                        </thead>
+                                        <tbody>
+                                          <tr>
+                                            <th scope="row">5</th>
+                                            <td>Mark</td>
+                                            <td>Otto</td>
+                                            <td>@mdo</td>
+                                          </tr>
+                                          <tr>
+                                            <th scope="row">6</th>
+                                            <td>Jacob</td>
+                                            <td>Thornton</td>
+                                            <td>@fat</td>
+                                          </tr>
+                                          <tr>
+                                            <th scope="row">7</th>
+                                            <td>Larry</td>
+                                            <td>the Bird</td>
+                                            <td>@twitter</td>
+                                          </tr>
+                                        </tbody>
+                                      </table>
+                                      <div>
+                                        <button class="btn btn-primary" type="submit">Supprimer salarieés</button>
+                                        <button class="btn btn-primary" type="submit">Susprendre l'accès</button>
+                                      </div>
+                                      
+
+
+
+
